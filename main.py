4ec24de70bf15b80673c72876492f30baab48ffc
asb <asbabushkin@yandex.ru>
2022-10-24 15:57:08 +0500

    Sheduled main function launch implemented.

--- main.py
+++ main.py
@@ -19,35 +19,8 @@ from telethon import TelegramClient, events, sync, connection as tel_connection
 from dotenv import load_dotenv
 from psycopg2.extensions import AsIs
 
-
 load_dotenv()
 options_chrome = webdriver.ChromeOptions()
-
-
-def get_price(url):
-    with webdriver.Chrome(options=options_chrome) as browser:
-        browser.get(url)
-        if WebDriverWait(browser, 100, poll_frequency=0.5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'c27ZC'))):
-            div = browser.find_element(By.CLASS_NAME, 'jC6yz')
-            return div.find_elements(By.CLASS_NAME, '_4-iO8')[1].text
-
-
-def send_result(or_city, des_city, dep_date, ret_date):
-    ua = UserAgent()
-    user_agent = ua.random
-    options_chrome.add_argument(f'user-agent={user_agent}')
-    price = get_price(search_link)
-    if ret_date is not None:
-        print(f'Перелет {or_city} - {des_city} {dep_date} - {ret_date} цена {price} руб.')
-        with TelegramClient('flight_catcher', int(os.getenv('TELEGRAM_API')), os.getenv('TELEGRAM_HASH')) as client:
-            client.send_message(os.getenv('TELEGRAM_USER_NAME'),
-                                message=f'Перелет {or_city} - {des_city}\nвылет {dep_date}\nвозвращение {ret_date}\nцена {price} руб.')
-    else:
-        print(f'Перелет {or_city} - {des_city} вылет {dep_date} цена {price} руб.')
-        with TelegramClient('flight_catcher', int(os.getenv('TELEGRAM_API')), os.getenv('TELEGRAM_HASH')) as client:
-            client.send_message(os.getenv('TELEGRAM_USER_NAME'),
-                                message=f'Перелет {or_city} - {des_city}\nвылет {dep_date}\nцена {price} руб.')
 
 
 def set_connection():
@@ -66,49 +39,46 @@ def set_connection():
         print('[INFO] Error while working with PostgreSQL', _ex)
         raise _ex
 
+
 def get_data(db_connection, table):
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM %(table_name)s;", {"table_name": AsIs(table)})
     return cursor.fetchall()
 
 
-if __name__ == '__main__':
+def get_price(url):
+    with webdriver.Chrome(options=options_chrome) as browser:
+        browser.get(url)
+        if WebDriverWait(browser, 100, poll_frequency=0.5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'c27ZC'))):
+            div = browser.find_element(By.CLASS_NAME, 'jC6yz')
+            return div.find_elements(By.CLASS_NAME, '_4-iO8')[1].text
 
+
+def send_result(or_city, des_city, dep_date, ret_date, search_link):
+    ua = UserAgent()
+    user_agent = ua.random
+    options_chrome.add_argument(f'user-agent={user_agent}')
+    price = get_price(search_link)
+    if ret_date is not None:
+        print(f'Перелет {or_city} - {des_city} {dep_date} - {ret_date} цена {price} руб.')
+        with TelegramClient('flight_catcher', int(os.getenv('TELEGRAM_API')), os.getenv('TELEGRAM_HASH')) as client:
+            client.send_message(os.getenv('TELEGRAM_USER_NAME'),
+                                message=f'Перелет {or_city} - {des_city}\nвылет {dep_date}\nвозвращение {ret_date}\nцена {price} руб.')
+    else:
+        print(f'Перелет {or_city} - {des_city} вылет {dep_date} цена {price} руб.')
+        with TelegramClient('flight_catcher', int(os.getenv('TELEGRAM_API')), os.getenv('TELEGRAM_HASH')) as client:
+            client.send_message(os.getenv('TELEGRAM_USER_NAME'),
+                                message=f'Перелет {or_city} - {des_city}\nвылет {dep_date}\nцена {price} руб.')
+
+
+def main():
     with set_connection() as conn:
         search_data = get_data(conn, 'flight_search_search')
-        values = get_data(conn, 'flight_search_citycode')
-
-
-    # try:
-    #     db_connection = ps2.connect(
-    #         host=os.getenv('db_host'),
-    #         user=os.getenv('db_user'),
-    #         password=os.getenv('db_password'),
-    #         port=os.getenv('db_port'),
-    #         database=os.getenv('db_name')
-    #     )
-    #     db_connection.autocommit = True
-    #
-    #     with db_connection.cursor() as cursor:
-    #         cursor.execute("SELECT * FROM flight_search_search;")
-    #         search_data = cursor.fetchall()
-    #         cursor.execute("SELECT * FROM flight_search_citycode;")
-    #         values = cursor.fetchall()
-    #         keys = ['city_eng', 'city_rus', 'code_eng', 'code_rus']
-    #
-    #
-    #
-    # except Exception as _ex:
-    #     print('[INFO] Error while working with PostgreSQL', _ex)
-    #
-    #
-    # finally:
-    #     if db_connection:
-    #         db_connection.close()
-    #         print('[INFO] PostgreSQL connection closed')
+        city_data = get_data(conn, 'flight_search_citycode')
 
     keys = ['city_eng', 'city_rus', 'code_eng', 'code_rus']
-    city_codes = [dict(zip(keys, values[v][1:])) for v in range(len(values))]
+    city_codes = [dict(zip(keys, city_data[c][1:])) for c in range(len(city_data))]
     for record in search_data:
         depart_date = str(record[3].day).rjust(2, '0') + str(record[3].month).rjust(2, '0')
         if record[4] is not None:
@@ -125,8 +95,12 @@ if __name__ == '__main__':
 
         search_link = f'https://www.onetwotrip.com/ru/f/search/{depart_date}{dest_city_code}{origin_city_code}{return_date}?s=true&sc=E&ac={num_adults}'
         print(search_link)
-        send_result(or_city=record[-2], des_city=record[-1], dep_date=record[3], ret_date=record[4])
+        send_result(or_city=record[-2], des_city=record[-1], dep_date=record[3], ret_date=record[4],
+                    search_link=search_link)
+    return True
 
-    # schedule.every().minute.do(main)
-    # while True:
-    #     schedule.run_pending()
+
+if __name__ == '__main__':
+    schedule.every().minute.do(main)
+    while True:
+        schedule.run_pending()
