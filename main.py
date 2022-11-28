dbdd61d4fea72e07074c7f2042731856cc239a73
asb <asbabushkin@yandex.ru>
2022-11-28 23:21:05 +0500

    Corrections after Flight_search_web db edition applied.

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_data(db_connection, table):
 def delete_depart_date_expired_records(db_connection, table, archive):
     cursor = db_connection.cursor()
     cursor.execute(
-        "INSERT INTO %(archive)s (old_id, oneway_flight, max_transhipments, depart_date, return_date, num_adults, num_children, luggage, search_init_date, telegr_acc, phone_num, email, depature_city, dest_city) SELECT * FROM %(table_name)s WHERE depart_date < CURRENT_DATE;",
+        "INSERT INTO %(archive)s (old_id, depart_city, dest_city, max_transhipments, depart_date, return_date, num_adults, num_children, num_infants, luggage, search_init_date, telegr_acc, phone_num, email) SELECT * FROM %(table_name)s WHERE depart_date < CURRENT_DATE;",
         {"archive": AsIs(archive), "table_name": AsIs(table)})
     cursor.execute("DELETE FROM %(table_name)s WHERE depart_date < CURRENT_DATE;",
                    {"table_name": AsIs(table)})
@@ -56,7 +56,7 @@ def delete_depart_date_expired_records(db_connection, table, archive):
 def delete_old_records(db_connection, table, archive):
     cursor = db_connection.cursor()
     cursor.execute(
-        "INSERT INTO %(archive)s (old_id, oneway_flight, max_transhipments, depart_date, return_date, num_adults, num_children, luggage, search_init_date, telegr_acc, phone_num, email, depature_city, dest_city) SELECT * FROM %(table_name)s WHERE EXTRACT(EPOCH FROM now() - search_init_date)/3600 > 72;",
+        "INSERT INTO %(archive)s (old_id, depart_city, dest_city, max_transhipments, depart_date, return_date, num_adults, num_children, num_infants, luggage, search_init_date, telegr_acc, phone_num, email) SELECT * FROM %(table_name)s WHERE EXTRACT(EPOCH FROM now() - search_init_date)/3600 > 72;",
         {"archive": AsIs(archive), "table_name": AsIs(table)})
     cursor.execute("DELETE FROM %(table_name)s WHERE EXTRACT(EPOCH FROM now() - search_init_date)/3600 > 72;",
                    {"table_name": AsIs(table)})
@@ -79,12 +79,13 @@ def get_flight_price(url):
     with webdriver.Chrome(seleniumwire_options=options, options=opts) as browser:
         #        browser.get('https://httpbin.org/user-agent')
         ##        time.sleep(5)
+        print('selenium',  url)
         browser.get(url)
         try:
             if WebDriverWait(browser, 20, poll_frequency=0.5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'c27ZC'))):
-                div = browser.find_element(By.CLASS_NAME, 'jC6yz')
-                return div.find_elements(By.CLASS_NAME, '_4-iO8')[-1].text
+                    EC.presence_of_element_located((By.CLASS_NAME, '_4eVQ4'))):
+                div = browser.find_element(By.CLASS_NAME, '_4eVQ4')
+                return div.find_elements(By.CLASS_NAME, '_4-iO8')[0].text
         except TimeoutException:
             return False
 
@@ -114,8 +115,8 @@ def send_result(or_city, des_city, dep_date, ret_date, price, telegram_user):
 
 def main():
     with set_connection() as conn:
-        delete_depart_date_expired_records(conn, 'flight_search_search', 'flight_search_archive')
-        delete_old_records(conn, 'flight_search_search', 'flight_search_archive')
+        delete_depart_date_expired_records(conn, 'flight_search_search', 'flight_search_searcharchive')
+        delete_old_records(conn, 'flight_search_search', 'flight_search_searcharchive')
         search_data = get_data(conn, 'flight_search_search')
         city_data = get_data(conn, 'flight_search_citycode')
 
@@ -123,25 +124,25 @@ def main():
     city_codes = [dict(zip(keys, city_data[c][1:])) for c in range(len(city_data))]
     for record in search_data:
         dest_city_code = origin_city_code = return_date = depart_date = num_adults = telegram_user = ''
-        depart_date = str(record[3].day).rjust(2, '0') + str(record[3].month).rjust(2, '0')
-        if record[4] is not None:
-            return_date = str(record[4].day).rjust(2, '0') + str(record[4].month).rjust(2, '0')
+        depart_date = str(record[4].day).rjust(2, '0') + str(record[4].month).rjust(2, '0')
+        if record[5] is not None:
+            return_date = str(record[5].day).rjust(2, '0') + str(record[5].month).rjust(2, '0')
         for i in city_codes:
-            if i['city_rus'] == record[-2]:
+            if i['city_rus'] == record[1]:
                 origin_city_code = i['code_eng']
                 break
         for i in city_codes:
-            if i['city_rus'] == record[-1]:
+            if i['city_rus'] == record[2]:
                 dest_city_code = i['code_eng']
                 break
-        num_adults = record[5]
-        telegram_user = record[9]
-        num_tranship = record[2]
+        num_tranship = record[3]
+        num_adults = record[6]
+        telegram_user = record[11]
         print(telegram_user)
-
         search_link = f'https://www.onetwotrip.com/ru/f/search/{depart_date}{origin_city_code}{dest_city_code}{return_date}?s=true&sc=E&ac={num_adults}&tr={num_tranship}'
+        print(search_link)
         price = get_flight_price(search_link)
-        send_result(or_city=record[-2], des_city=record[-1], dep_date=record[3], ret_date=record[4],
+        send_result(or_city=record[1], des_city=record[2], dep_date=record[4], ret_date=record[5],
                     price=price, telegram_user=telegram_user)
     return True
 
