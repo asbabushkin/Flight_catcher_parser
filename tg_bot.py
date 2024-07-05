101bb844195496ac64da92b0ec04b39f91e1c7e7
asb <asbabushkin@yandex.ru>
2024-07-05 23:05:43 +0500

    Revert "ruff checked"
    This reverts commit 2f0b2d23eeb782a42c87171eb8857b6dc574cf72.

--- tg_bot.py
+++ tg_bot.py
@@ -2,16 +2,15 @@
 Aiogram & asyncio version.
 Not using in the project.
 """
-
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor, types
-
-from data import telegram_user_id, token, url_ottrip
+from data import token, telegram_user_id, url_ottrip
 from main import get_flight_price_selenium
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
+
 
 
 async def check_price_every_minute():
@@ -21,7 +20,7 @@ async def check_price_every_minute():
         await asyncio.sleep(30)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(check_price_every_minute())
     executor.start_polling(dp)
